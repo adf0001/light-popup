dc986df373b880be6093ec09099b4dc419a64d30
fwg <adf0001@163.com>
2022-08-18 15:04:48 +0800

    add: common items

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ Object.assign(exports,
 	require("./lib/show-html"),
 	require("./lib/hide"),
 
-	//common,
+	//common
+	require("./lib/common/alert"),
+	require("./lib/common/confirm"),
+	require("./lib/common/prompt"),
+	require("./lib/common/select-radio-list"),
+	require("./lib/common/select-checkbox-list"),
+	require("./lib/common/select-button-list"),
+
 );
--- test/test-data.js
+++ test/test-data.js
@@ -38,6 +38,41 @@ module.exports = {
 		return 'ui test';
 	},
 
+	".showHtml()": function (done) {
+		_ele('divResult2').innerHTML =
+			"<label><input id='chkModaless' type='checkbox'></input>modal ( default modeless)</label><br>" +
+			"<button id='btnOpenPopup1' onclick=\"light_popup.showHtml('title-1<hr>message-1',{modal:_ele('chkModaless').checked,})\">.showHtml()</button> " +
+			
+			"<button id='btnOpenPopup2' onclick=\"light_popup.alert('message-2, <span name=ok>ok test</span>',{modal:_ele('chkModaless').checked,} )\">alert()</button> " +
+			"<button id='btnOpenPopup2' onclick=\"light_popup.alert('message-2, <span name=ok>ok test</span>',{modal:_ele('chkModaless').checked,cb:(err,data)=>{alert([err,data]);},cbEvent:(err,data)=>{console.log(err,data);},maximizeButton:true} )\">alert() cb/event</button> " +
+
+			"<button id='btnOpenPopup3' onclick=\"light_popup.confirm('message-3',{modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">confirm()</button> " +
+			"<button id='btnOpenPopup3' onclick=\"light_popup.confirm('message-3 message-3 message-3 message-3',{modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">confirm()-2</button> " +
+			"<button id='btnOpenPopup4' onclick=\"light_popup.confirmYnc('message-4',{modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">confirmYnc()</button> " +
+			"<button id='btnOpenPopup5' onclick=\"light_popup.prompt('message-5',{defaultValue:'default-value',modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">prompt()</button> " +
+			"<button id='btnOpenPopup5' onclick=\"light_popup.prompt('add new project path',{defaultValue:'default-value',modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">prompt()-2</button> " +
+			"<button id='btnOpenPopupS' onclick=\"light_popup.showHtml(popupHtmlStackHtml+'<br>'+(new Date()),_ele('chkModaless').checked)\">.showHtml-stack</button> " +
+
+			"<button id='btnOpenPopupSelect' onclick=\"light_popup.selectRadioList('select message 1',{itemList:['aaa',['bbb','文本bbb'],'ccc'],defaultValue:'bbb',modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">selectRadioList()</button> " +
+			"<button id='btnOpenPopupSelect-2' onclick=\"light_popup.selectRadioList('select message 1',{itemList:['aaa',['bbb','文本bbb'],'ccc'],modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\" title='if select void, exception raise and stop the process'>selectRadioList()-2/void</button> " +
+
+			"<button id='btnOpenPopupSelect2' onclick=\"light_popup.selectCheckboxList('select message 2',{itemList:['aaa',['bbb','文本bbb'],'ccc'],defaultValueList:['bbb','ccc'],modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">selectCheckboxList()</button> " +
+			"<button id='btnOpenPopupSelect2-2' onclick=\"light_popup.selectCheckboxList('select message 2',{itemList:['aaa',['bbb','文本bbb'],'ccc'],modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\" title='if select void, return empty list'>selectCheckboxList()-2/void</button> " +
+			"<button id='btnOpenPopupSelect2-3' onclick=\"light_popup.selectCheckboxList('select message 2',{itemList:['aaa',['bbb','文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb '],'ccc','ccc2'," +
+			"		'ccc3','ccc4','-','ccc5-sep','ccc6',null,'ccc7-skip null','ccc8','ccc9','ccc10','ccc11','ccc12','ccc13','ccc14'],defaultValueList:['bbb','ccc'],modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">selectCheckboxList()-3</button> " +
+
+			"<button id='btnOpenPopupSelect2-4' onclick=\"light_popup.selectButtonList('select message 3',{itemList:['aaa',['bbb','文本bbb'],'ccc'],modal:_ele('chkModaless').checked,cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">selectButtonList()</button> " +
+			"<button id='btnOpenPopupSelect2-5' onclick=\"light_popup.selectButtonList('select message 2',{itemList:['aaa',['bbb','文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb 文本bbb '],'ccc',['ccc2','<div style=\\'color:red;text-align:left;\\'>ccc2</div>','cursor:wait;',{disabled:true}],'ccc3'," +
+			"		'ccc4','ccc5','-','ccc6-sep',['ccc7',null,'color:orange;text-align:right;'],null,'ccc8-skip null','ccc9','ccc10','ccc11','ccc12','ccc13','ccc14'],modal:_ele('chkModaless').checked,maxHeight:'15em',cb:function(err,data){if(err||data)alert('returned: error='+err+', data='+data);}})\">selectButtonList()-2</button> " +
+			"";
+
+		window.popupHtmlStackHtml = "title-S<hr>message-S <span class='-ht-cmd' onclick='openPopupHtmlStack()'>open another</span>";
+
+		window.openPopupHtmlStack = function () { _ele('btnOpenPopupS').click(); };
+
+		return 'ui test';
+	},
+
 	"check exports": function (done) {
 		for (var i in light_popup) {
 			if (typeof light_popup[i] === "undefined") { done("undefined: " + i); return; }
